c080eb8ee995e66792f3d2c4c1bb6d70b861e30b
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-07-19 13:13:14 -0300

    Backend project created

---
+++ backend/src/main/java/com/vnoogueira/dscatolog/DscatologApplication.java
@@ -0,0 +1,13 @@
+package com.vnoogueira.dscatolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatologApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vnoogueira/dscatolog/DscatologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnoogueira.dscatolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
