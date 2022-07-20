77d79d29e81891ea5da80a1cb35ad25a1b15f17b
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-07-20 13:43:36 -0300

    Search category by id

--- backend/src/main/java/com/vnoogueira/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/vnoogueira/dscatolog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.vnoogueira.dscatolog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,14 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+		
 	}
 
 }
