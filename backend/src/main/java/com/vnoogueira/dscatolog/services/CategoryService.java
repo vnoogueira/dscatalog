ad05cecb4d81325177f30f285f0a804e7105aedc
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-07-19 21:08:16 -0300

    Category DTO

--- backend/src/main/java/com/vnoogueira/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/vnoogueira/dscatolog/services/CategoryService.java
@@ -1,23 +1,28 @@
 package com.vnoogueira.dscatolog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vnoogueira.dscatolog.dto.CategoryDTO;
 import com.vnoogueira.dscatolog.entities.Category;
 import com.vnoogueira.dscatolog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();		
+	public List<CategoryDTO> findAll() {
+		
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
